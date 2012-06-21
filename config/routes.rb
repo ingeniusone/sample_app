60c122a44a2fb8c0faa2472e260f8caaa59d1f68
ingeniusone <shazia.sami@gmail.com>
2012-06-21 19:56:50 -0400

    added sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+  
+  match '/signup',  :to => 'users#new'
+  
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  match '/pages' => 'pages#home'
+  
+  root :to => 'pages#home'
+ 
   
   
     
