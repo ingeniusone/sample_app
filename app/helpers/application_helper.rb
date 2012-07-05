598573d528208e0e5b3392bcedd5673052f1f2a8
ingeniusone <shazia.sami@gmail.com>
2012-07-05 14:25:43 -0400

    have not committed in a long time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,7 @@ module ApplicationHelper
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
+      base_title
     else
       "#{base_title} | #{@title}"
       end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,41 +2,37 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
-
+ end
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-    
+  end  
     it "should have the right title" do
       get 'home'
       response.should contain("Ruby on Rails Tutorial Sample App | Home")
     end    
-  end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
+   end  
     
     it "should have the right title" do
       get 'contact'
       response.should contain("Ruby on Rails Tutorial Sample App | Contact")
     end
-  end
   
 describe "GET 'about' " do
   it "should be successful" do
     get 'about'
     response.should be_success
   end
-  
+ end
   it "should have the right title" do
     get 'about'
     response.should contain("Ruby on Rails Tutorial Sample App | About")
   end
-end    
-
-end
