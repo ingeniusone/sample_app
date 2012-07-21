1ec97287e5f49ee65dd6f18bf220b44e7a0cabab
ingeniusone <shazia.sami@gmail.com>
2012-07-21 16:30:57 -0400

    changes through section 7.1.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessible :name, :email :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -8,4 +8,21 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format   => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+                    
+  # Automatically create the virtual attribute 'password_confirmation'.
+  validates :password, :presence    => true,
+            :confirmation           => true,
+            :length                 => { :within => 6..40 }
+            
+      before_save :encrypt_password
+
+      private
+
+          def encrypt_password
+            self.encrypted_password = encrypt(password)
+         end
+
+          def encrypt(string)
+            string # Only a temporary implementation!
+          end
 end
